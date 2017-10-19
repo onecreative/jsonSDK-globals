9cc5095a9e8bc333f87daa90a151466b842bb055
Adam Cook <adam@onecreative.pro>
2017-10-18 22:23:43 -0700

    first push

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ exports.jsonGlobals = function (req,res) {
 			jsonGlobals._appID = req.body.appID;
 			jsonGlobals.builderPrefix = origin.replace(/-[0-9]*-apps.worldsecuresystems.com/,'');
 			jsonGlobals.builderPath = referer.match(/\/_system\/apps\/.*?\//)[0];
+			if (jsonGlobals.builderPath !== null) jsonGlobals.builderPath = jsonGlobals.builderPath[0];
 			jsonGlobals.utilitiesPath = jsonGlobals.builderPath+'public/utilities/',
 			jsonGlobals.appPath = jsonGlobals.builderPath+'public/apps/'+req.body.appID+'/',
 			jsonGlobals.jsonPath = jsonGlobals.builderPath+'public/apps/'+req.body.appID+'/json/',
