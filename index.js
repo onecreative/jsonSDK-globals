5034a01738500b1dae8826fd404938dcc9a6431c
Adam Cook <adam@onecreative.pro>
2017-10-18 22:22:05 -0700

    first push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var jsonGlobals = require('./globals');
 
 exports.jsonGlobals = function (req,res) {
 	var origin = req.get('Origin') || '';
+	var referer = req.get('Referer') || '';
 	//set JSON content type and CORS headers for the response
 	res.header('Content-Type','application/json');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -16,7 +17,7 @@ exports.jsonGlobals = function (req,res) {
 		if ([undefined,''].indexOf(req.body.appID) === -1) {
 			jsonGlobals._appID = req.body.appID;
 			jsonGlobals.builderPrefix = origin.replace(/-[0-9]*-apps.worldsecuresystems.com/,'');
-			jsonGlobals.builderPath = req.get('Referer').match(/\/_system\/apps\/.*?\//)[0];
+			jsonGlobals.builderPath = referer.match(/\/_system\/apps\/.*?\//)[0];
 			jsonGlobals.utilitiesPath = jsonGlobals.builderPath+'public/utilities/',
 			jsonGlobals.appPath = jsonGlobals.builderPath+'public/apps/'+req.body.appID+'/',
 			jsonGlobals.jsonPath = jsonGlobals.builderPath+'public/apps/'+req.body.appID+'/json/',
