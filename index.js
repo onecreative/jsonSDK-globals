10cd0352fcec2ff2a6b3c3a52e4a1b28b7501e1c
Adam Cook <adam@onecreative.pro>
2017-10-18 22:05:52 -0700

    first push

---
+++ index.js
@@ -0,0 +1,33 @@
+var jsonGlobals = require('./globals');
+
+exports.jsonGlobals = function globals (req,res) {
+	//set JSON content type and CORS headers for the response
+	res.header('Content-Type','application/json');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	if (typeof apiKeys[req.get('Origin')] !== 'undefined') res.header('Access-Control-Allow-Origin', req.get('Origin'));
+
+	//respond to CORS preflight requests
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', '');
+		res.status(204).send('');
+	}else {
+		//continue on
+		if ([undefined,''].indexOf(req.body.appID) === -1) {
+			jsonGlobals._appID = req.body.appID;
+			jsonGlobals.builderPrefix = req.get('Origin').replace(/-[0-9]*-apps.worldsecuresystems.com/,'');
+			jsonGlobals.builderPath = req.get('Referer').match(/\/_system\/apps\/.*?\//)[0];
+			jsonGlobals.utilitiesPath = jsonGlobals.builderPath+'public/utilities/',
+			jsonGlobals.appPath = jsonGlobals.builderPath+'public/apps/'+req.body.appID+'/',
+			jsonGlobals.jsonPath = jsonGlobals.builderPath+'public/apps/'+req.body.appID+'/json/',
+			jsonGlobals.uploadPath = jsonGlobals.builderPath+'public/apps/'+req.body.appID+'/uploads/',
+			jsonGlobals.tempPath = jsonGlobals.builderPath+'public/apps/'+req.body.appID+'/json/temp/',
+			jsonGlobals.destinationPrefix = 'APP_'+req.body.appID+'_',
+			jsonGlobals.tempName = 'APP_'+req.body.appID+'_temp'
+			res.send(jsonGlobals);
+		}else {
+			var errors = {};
+			if ([undefined,''].indexOf(req.body.appID) > -1) errors.appID = 'Your appID was not included in your request.';
+			res.send(JSON.stringify(errors));
+		}
+	}
+};
